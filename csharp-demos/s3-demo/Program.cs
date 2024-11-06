b2a348c6d2278d3ddc29d6bc7bac7205a738705a
Mahendra Shinde <MahendraShinde@synegertics-india.com>
2024-11-06 12:13:02 +0530

    Dotnet Sample list-s3

---
+++ csharp-demos/s3-demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Threading.Tasks;
+
+namespace S3Demo
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var s3Client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
+
+            try
+            {
+                var response = await s3Client.ListBucketsAsync();
+                Console.WriteLine("Buckets:");
+                foreach (S3Bucket bucket in response.Buckets)
+                {
+                    Console.WriteLine(bucket.BucketName);
+                }
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when listing buckets", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when listing buckets", e.Message);
+            }
+        }
+    }
+}
